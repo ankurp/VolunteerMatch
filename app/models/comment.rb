3b65f952c5889377549ab6350ca2e217cae09c60
Ankur Patel <ankur@encoredevlabs.com>
2020-06-19 15:03:28 -0400

    Combining broadcast

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,14 +4,10 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  after_create :notify_organizer
-  after_save :broadcast
+  after_create :notify_organizer_and_broadcast
 
-  def notify_organizer
+  def notify_organizer_and_broadcast
     Notification.post(to: self.post.created_by, from: self.user, action: :post_comment, notifiable: self)
-  end
-
-  def broadcast
     cable_ready['comment'].insert_adjacent_html(
       selector: '.new-comment-placeholder',
       position: 'afterbegin',
