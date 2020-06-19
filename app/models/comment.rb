867602cc5d029403d280880b7538ebdc6232b6e0
Ankur Patel <ankur@encoredevlabs.com>
2020-06-19 14:59:42 -0400

    Moving logic to model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,25 @@
 class Comment < ApplicationRecord
+  include CableReady::Broadcaster
+
   belongs_to :post
   belongs_to :user
 
   after_create :notify_organizer
+  after_save :broadcast
 
   def notify_organizer
     Notification.post(to: self.post.created_by, from: self.user, action: :post_comment, notifiable: self)
+  end
+
+  def broadcast
+    cable_ready['comment'].insert_adjacent_html(
+      selector: '.new-comment-placeholder',
+      position: 'afterbegin',
+      html: ApplicationController.render(
+        partial: "comments/comment",
+        locals: { comment: self }
+      )
+    )
+    cable_ready.broadcast
   end
 end
